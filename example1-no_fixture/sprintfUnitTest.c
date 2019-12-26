3a063678be72bd63b0630227809d2dffee69aa31
MartinPereiraN <m.pereira.n@hotmail.com.ar>
2019-12-25 21:45:04 -0300

    base example, no fixture.

---
+++ example1-no_fixture/sprintfUnitTest.c
@@ -0,0 +1,21 @@
+#include "unity.h"
+#include "productionCode1.h"
+
+void setUp (void) {}; /* Is run before every test, put unit init calls here. */
+void tearDown (void) {}; /* Is run after every test, put unit clean-up calls here. */
+
+void test_sprintf_(void)
+{
+char output[5] = "";
+TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
+TEST_ASSERT_EQUAL_STRING("hey", output);
+}
+
+int main(void)
+{
+UNITY_BEGIN();
+
+RUN_TEST(test_sprintf);
+
+return UNITY_END();
+}
